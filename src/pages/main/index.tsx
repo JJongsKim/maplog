f47954c1a5e02785e849fcd362dc187d123785ff
JJongsKim <totnwjd530@gmail.com>
2022-10-18 16:46:39 +0900

    style: 메인페이지 전체적인 스타일링 완료

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 import Layout from '../../components/layout';
 import MainFirst from '../../components/mainPage/mainFirst';
 import MainSecond from '../../components/mainPage/mainSecond';
@@ -6,11 +8,19 @@ import MainThird from '../../components/mainPage/mainThird';
 const Main = () => {
   return (
     <Layout>
-      <MainFirst />
-      <MainSecond />
-      <MainThird />
+      <MainContainer>
+        <MainFirst />
+        <MainSecond />
+        <MainThird />
+      </MainContainer>
     </Layout>
   );
 };
+
+const MainContainer = styled.div`
+  /* width: 1512px;
+  height: 982px;
+  overflow: hidden; */
+`;
 
 export default Main;
