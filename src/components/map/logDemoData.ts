a9aa4549163694254f24f2ac9233aed2530ddeb3
JJongsKim <totnwjd530@gmail.com>
2022-11-09 19:21:42 +0900

    feat: 모아보기 페이지 데모데이터 추가, 위치별 마커 표시

---
+++ src/components/map/logDemoData.ts
@@ -0,0 +1,18 @@
+export const logDemoData = [
+  {
+    latlng: new kakao.maps.LatLng(37.60145647863699, 127.08021681729517),
+    log: {
+      title: '중화역 짱맛탱 스시집',
+      logDate: '2022-11-09',
+      logContent: '아 여기 쮸니랑 줄 서서 먹었는데 진짜 맛있었음',
+    },
+  },
+  {
+    latlng: new kakao.maps.LatLng(37.592330126931245, 127.0178030250958),
+    log: {
+      title: '맨날 카공하는 곳',
+      logDate: '2022-11-09',
+      logContent: '쮸니랑 성신에서 카공할 때 마다 오는 탐탐스~',
+    },
+  },
+];
---
+++ src/components/map/logmapContainer.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { ModalState } from '../../atoms/ModalState';
+
+import UpdateModal from '../../pages/updateModal';
+import Portal from '../Portal';
+
+import { logDemoData } from './logDemoData';
+
+const logmapContainer = () => {
+  const [isModal, setIsModal] = useRecoilState(ModalState);
+
+  useEffect(() => {
+    const mapContainer = document.getElementById('map');
+    const mapOption = {
+      center: new kakao.maps.LatLng(37.5666805, 126.9784147),
+      level: 8,
+    };
+
+    const logmap = new kakao.maps.Map(mapContainer, mapOption);
+
+    const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png';
+
+    for (let i = 0; i < logDemoData.length; i += 1) {
+      // 마커 이미지의 이미지 크기 입니다
+      const imageSize = new kakao.maps.Size(35, 40);
+
+      // 마커 이미지를 생성합니다
+      const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+      // 마커를 생성합니다
+      const marker = new kakao.maps.Marker({
+        map: logmap, // 마커를 표시할 지도
+        position: logDemoData[i].latlng, // 마커를 표시할 위치
+        image: markerImage, // 마커 이미지
+      });
+
+      kakao.maps.event.addListener(marker, 'click', function () {
+        setIsModal(!isModal);
+      });
+    }
+  }, []);
+
+  return (
+    <MapWrap id="map">
+      {isModal && (
+        <Portal>
+          <UpdateModal />
+        </Portal>
+      )}
+    </MapWrap>
+  );
+};
+
+const MapWrap = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
+
+export default logmapContainer;
