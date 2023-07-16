1ccfc57f9d03ac3735c46caba33a21a64d2313d5
JJongsKim <totnwjd530@gmail.com>
2023-07-16 14:49:03 +0900

    :heavy_plus_sign: setting: eslint/prettier, 라이브러리 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
